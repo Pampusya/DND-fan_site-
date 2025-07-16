c5c065225a729270a722be2f45b48eaa24c97b0c
Pampusya <55555oo55555000@gmail.com>
2025-07-17 00:54:41 +0300

    clean_up

---
+++ app/global/background/background.tsx
@@ -0,0 +1,7 @@
+// global/background/background.tsx
+import React from "react";
+import "./background_style.scss";
+
+export function Background() {
+  return <div className="bg-stripe" />;
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"), 
-    route("test", "routes/test_page.tsx")
+    index("routes/home_page.tsx"), 
+    //route("test", "routes/test_page.tsx")
 ] satisfies RouteConfig;
